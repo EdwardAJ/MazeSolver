2bac5b4ff4a364fda2c8b1d076d42431fbb83b46
Elvina Ang <38122995+ryukago@users.noreply.github.com>
2019-03-27 18:56:12 +0700

    file external added
    bisa input dari file external

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ def getMazeFromKeyboard(mat,baris,kolom):
 #fungsi mendapat maze dari file
 #parameter : list of list mat
 def getMazeFromFile(mat):
-
+    namaFile = input("Nama File : ")
+    file = open(namaFile, 'r')
+    for baris in file:
+        x = baris[:len(baris)-1]
+        mat.append(list(x))
 
 
 #program utama
 def main():
     #pilihan untuk memasukkan maze dari file external atau dari keyboard.
-    print("Ketik 0 untuk file eksternal, Ketik 1 dari keyboard: ")
+    print("Ketik 1 untuk file eksternal, Ketik 0 dari keyboard: ")
     input_param = int(input())
     if (input_param == 0):
         baris = int (input("Masukkan jumlah baris dari Map: "))
@@ -27,8 +31,32 @@ def main():
         getMazeFromKeyboard(mat,baris,kolom)
         print(mat)
     elif (input_param == 1):
+        mat = []
+        getMazeFromFile(mat)
+        print(mat)
 
-
+# def bfs(arr, temp, i, j, ifinal, jfinal):
+#     arr[i] = arr[i][:j] + '2' + arr[i][j+1:]
+#     print(arr)
+#     if ((i == ifinal) and (j == jfinal)):
+#         temp.append([i,j])
+#         return temp
+#     else:
+#         if (arr[i][j+1] == '0'):
+#             temp.append([i,j])
+#             return bfs(arr, temp, i, j+1, ifinal, jfinal)
+#         elif (arr[i+1][j] == '0'):
+#             temp.append([i,j])
+#             return bfs(arr, temp, i+1, j, ifinal, jfinal)
+#         elif (arr[i][j-1] == '0'):
+#             temp.append([i,j])
+#             return bfs(arr, temp, i, j-1, ifinal, jfinal)
+#         elif (arr[i-1][j] == '0'):
+#             temp.append([i,j])
+#             return bfs(arr, temp, i-1, j-1, ifinal, jfinal)
+#         else:
+#             temp = []
+#             return (temp)
 
 if __name__ == '__main__':
     main()
