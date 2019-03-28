f41b46a445330bfdee24149cc3c441fdc3be965d
EdwardAJ <edward.jaya31@gmail.com>
2019-03-28 22:54:16 +0700

    Adding Astar(not finished yet) and new functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#import matplotlib.pyplot as plt
 #Tugas Kecil Strategi Algoritma 2019
 #kelas InfoArrElement, menyimpan posisi elemen,  "bapak" dari elemen, dan cost.
 class InfoArrElement:
@@ -30,7 +31,6 @@ def getMazeFromFile(mat, namaFile):
 
 #queue, variabel global, menyimpan status parent dan cost
 queue_infoMat = []
-
 #fungsi BFS, menggunakan struktur data queue
 def BFS(mat, mat_visited, i_awal , j_awal, i_goal, j_goal):
     path = InfoArrElement(i_awal, j_awal, None, 0)
@@ -49,8 +49,7 @@ def BFS(mat, mat_visited, i_awal , j_awal, i_goal, j_goal):
             if(not isIndexOutOfRange(path.i+1,path.j,mat)):
                 if (mat[path.i+1][path.j] == '0' and mat_visited[path.i+1][path.j] == False):
                     mat_visited[path.i][path.j] = True
-                    nextpath = InfoArrElement((path.i+1),(path.j),path,0)
-                    queue_infoMat.append(nextpath)
+                    queue_infoMat.append(InfoArrElement((path.i+1),(path.j),path,0))
             #cek kanan
             if(not isIndexOutOfRange(path.i,path.j+1,mat)):
                 if (mat[path.i][path.j+1] == '0' and mat_visited[path.i][path.j+1] == False):
@@ -67,14 +66,18 @@ def BFS(mat, mat_visited, i_awal , j_awal, i_goal, j_goal):
                     mat_visited[path.i][path.j] = True
                     queue_infoMat.append(InfoArrElement((path.i),(path.j-1),path,0))
 
+
 def Astar(mat, mat_visited, i_awal , j_awal, i_goal, j_goal):
-    path = InfoArrElement(i_awal, j_awal, None, 0)
+    g_cost = 0
+    h_cost = manhattanCost(i_awal,j_awal,i_goal,j_goal)
+    cost = g_cost + h_cost
+    path = InfoArrElement(i_awal, j_awal, None, cost)
     queue_infoMat.append(path)
     #isNotEmpty
     while (queue_infoMat):
+        #queue_infoMat.sort(key = path.cost)
         #proses elemen pertama dan hapus elemen pertama dalam queue
         path = queue_infoMat[0];  del queue_infoMat[0]
-
         #print(str(path.i) + "," + str(path.j))
         if (path.i == i_goal and path.j == j_goal):
             mat_visited[path.i][path.j] = True
@@ -83,24 +86,39 @@ def Astar(mat, mat_visited, i_awal , j_awal, i_goal, j_goal):
             #cek bawah
             if(not isIndexOutOfRange(path.i+1,path.j,mat)):
                 if (mat[path.i+1][path.j] == '0' and mat_visited[path.i+1][path.j] == False):
+                    g_cost = path.cost - manhattanCost(path.i,path.j,i_goal,j_goal) +1
+                    h_cost = manhattanCost(path.i+1,path.j,i_goal,j_goal)
+                    cost = g_cost + h_cost
                     mat_visited[path.i][path.j] = True
-                    nextpath = InfoArrElement((path.i+1),(path.j),path,0)
-                    queue_infoMat.append(nextpath)
+                    queue_infoMat.append(InfoArrElement((path.i+1),(path.j),path,cost))
             #cek kanan
             if(not isIndexOutOfRange(path.i,path.j+1,mat)):
                 if (mat[path.i][path.j+1] == '0' and mat_visited[path.i][path.j+1] == False):
+                    g_cost = path.cost - manhattanCost(path.i,path.j,i_goal,j_goal) +1
+                    h_cost = manhattanCost(path.i+1,path.j,i_goal,j_goal)
+                    cost = g_cost + h_cost
                     mat_visited[path.i][path.j] = True
-                    queue_infoMat.append(InfoArrElement((path.i),(path.j+1),path,0))
+                    queue_infoMat.append(InfoArrElement((path.i),(path.j+1),path,cost))
             #cek atas
             if(not isIndexOutOfRange(path.i-1,path.j,mat)):
                 if (mat[path.i-1][path.j] == '0' and mat_visited[path.i-1][path.j] == False):
+                    g_cost = path.cost - manhattanCost(path.i,path.j,i_goal,j_goal) +1
+                    h_cost = manhattanCost(path.i+1,path.j,i_goal,j_goal)
+                    cost = g_cost + h_cost
                     mat_visited[path.i][path.j] = True
-                    queue_infoMat.append(InfoArrElement((path.i-1),(path.j),path,0))
+                    queue_infoMat.append(InfoArrElement((path.i-1),(path.j),path,cost))
             #cek kiri
             if(not isIndexOutOfRange(path.i,path.j-1,mat)):
                 if (mat[path.i][path.j-1] == '0' and mat_visited[path.i][path.j-1] == False):
+                    g_cost = path.cost - manhattanCost(path.i,path.j,i_goal,j_goal) +1
+                    h_cost = manhattanCost(path.i+1,path.j,i_goal,j_goal)
+                    cost = g_cost + h_cost
                     mat_visited[path.i][path.j] = True
-                    queue_infoMat.append(InfoArrElement((path.i),(path.j-1),path,0))
+                    queue_infoMat.append(InfoArrElement((path.i),(path.j-1),path,cost))
+
+def manhattanCost(i,j,i_goal,j_goal):
+    cost = abs(i_goal - i) + abs(j_goal - j)
+    return cost
 
 def isIndexOutOfRange(i,j,mat):
     if (i >= 0 and i < len(mat) and j >= 0 and j < len(mat[0])):
@@ -122,7 +140,8 @@ def main():
         namaFile = input(str("Nama File : "))
         getMazeFromFile(mat, namaFile)
     #inisialisasi mat_visited, yaitu list of list berisi boolean visitedTrue atau visitedFalse.
-    mat_visited = [[False]*len(mat[0]) for idx_baris in range(len(mat))]
+    mat_visitedBFS = [[False]*len(mat[0]) for idx_baris in range(len(mat))]
+    mat_visitedAstar = [[False]*len(mat[0]) for idx_baris in range(len(mat))]
     #cari posisi jalan masuk
     for idx_baris in range(len(mat)):
         if (mat[idx_baris][0] == '0'):
@@ -134,7 +153,7 @@ def main():
             j_goal = len(mat[idx_baris])-1 ; i_goal = idx_baris
             break
     #lakukan BFS
-    path = BFS(mat, mat_visited, i_awal , j_awal, i_goal, j_goal)
+    path = BFS(mat, mat_visitedBFS, i_awal , j_awal, i_goal, j_goal)
     #Dapatkan Path
     while (path != None):
         mat[path.i][path.j] = 2
@@ -145,6 +164,8 @@ def main():
             print(mat[i][j], end = ' ')
         print()
         print()
+
+    path2 = Astar(mat, mat_visitedAstar, i_awal , j_awal, i_goal, j_goal)
 
 if __name__ == '__main__':
     main()
