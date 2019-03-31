6542d9f7f9f4dda411ad8b2c719a057e0c0d0b0b
EdwardAJ <edward.jaya31@gmail.com>
2019-03-31 19:34:57 +0700

    Adding GOOD COMPARATION and fix bug

--- main.py
+++ main.py
@@ -35,35 +35,37 @@ queue_infoMat = []
 def BFS(mat, mat_visited, i_awal , j_awal, i_goal, j_goal):
     path = InfoArrElement(i_awal, j_awal, None, 0)
     queue_infoMat.append(path)
+    count = 0
     #isNotEmpty
     while (queue_infoMat):
+        count = count + 1
         #proses elemen pertama dan hapus elemen pertama dalam queue
         path = queue_infoMat[0];  del queue_infoMat[0]
 
-        #print(str(path.i) + "," + str(path.j))
+        mat_visited[path.i][path.j] = True
         if (path.i == i_goal and path.j == j_goal):
-            mat_visited[path.i][path.j] = True
+            print("Iterasi BFS: " , count)
             return path
         else:
             #cek bawah
             if(not isIndexOutOfRange(path.i+1,path.j,mat)):
                 if (mat[path.i+1][path.j] == '0' and mat_visited[path.i+1][path.j] == False):
-                    mat_visited[path.i][path.j] = True
+                    #mat_visited[path.i][path.j] = True
                     queue_infoMat.append(InfoArrElement((path.i+1),(path.j),path,0))
             #cek kanan
             if(not isIndexOutOfRange(path.i,path.j+1,mat)):
                 if (mat[path.i][path.j+1] == '0' and mat_visited[path.i][path.j+1] == False):
-                    mat_visited[path.i][path.j] = True
+                    #mat_visited[path.i][path.j] = True
                     queue_infoMat.append(InfoArrElement((path.i),(path.j+1),path,0))
             #cek atas
             if(not isIndexOutOfRange(path.i-1,path.j,mat)):
                 if (mat[path.i-1][path.j] == '0' and mat_visited[path.i-1][path.j] == False):
-                    mat_visited[path.i][path.j] = True
+                    #mat_visited[path.i][path.j] = True
                     queue_infoMat.append(InfoArrElement((path.i-1),(path.j),path,0))
             #cek kiri
             if(not isIndexOutOfRange(path.i,path.j-1,mat)):
                 if (mat[path.i][path.j-1] == '0' and mat_visited[path.i][path.j-1] == False):
-                    mat_visited[path.i][path.j] = True
+                    #mat_visited[path.i][path.j] = True
                     queue_infoMat.append(InfoArrElement((path.i),(path.j-1),path,0))
 
 
@@ -71,16 +73,23 @@ def Astar(mat, mat_visited, i_awal , j_awal, i_goal, j_goal):
     g_cost = 0
     h_cost = manhattanCost(i_awal,j_awal,i_goal,j_goal)
     cost = g_cost + h_cost
+    #deklarasi kelas path
     path = InfoArrElement(i_awal, j_awal, None, cost)
     queue_infoMat.append(path)
+
+    count = 0
+
     #isNotEmpty
     while (queue_infoMat):
-        #queue_infoMat.sort(key = path.cost)
+        queue_infoMat.sort(key=lambda path: path.cost)
+        count = count + 1
+
         #proses elemen pertama dan hapus elemen pertama dalam queue
         path = queue_infoMat[0];  del queue_infoMat[0]
-        #print(str(path.i) + "," + str(path.j))
+        mat_visited[path.i][path.j] = True
+
         if (path.i == i_goal and path.j == j_goal):
-            mat_visited[path.i][path.j] = True
+            print("Iterasi A-Star: " , count)
             return path
         else:
             #cek bawah
@@ -89,31 +98,31 @@ def Astar(mat, mat_visited, i_awal , j_awal, i_goal, j_goal):
                     g_cost = path.cost - manhattanCost(path.i,path.j,i_goal,j_goal) +1
                     h_cost = manhattanCost(path.i+1,path.j,i_goal,j_goal)
                     cost = g_cost + h_cost
-                    mat_visited[path.i][path.j] = True
+                    #mat_visited[path.i][path.j] = True
                     queue_infoMat.append(InfoArrElement((path.i+1),(path.j),path,cost))
             #cek kanan
             if(not isIndexOutOfRange(path.i,path.j+1,mat)):
                 if (mat[path.i][path.j+1] == '0' and mat_visited[path.i][path.j+1] == False):
                     g_cost = path.cost - manhattanCost(path.i,path.j,i_goal,j_goal) +1
-                    h_cost = manhattanCost(path.i+1,path.j,i_goal,j_goal)
+                    h_cost = manhattanCost(path.i,path.j+1,i_goal,j_goal)
                     cost = g_cost + h_cost
-                    mat_visited[path.i][path.j] = True
+                    #mat_visited[path.i][path.j] = True
                     queue_infoMat.append(InfoArrElement((path.i),(path.j+1),path,cost))
             #cek atas
             if(not isIndexOutOfRange(path.i-1,path.j,mat)):
                 if (mat[path.i-1][path.j] == '0' and mat_visited[path.i-1][path.j] == False):
                     g_cost = path.cost - manhattanCost(path.i,path.j,i_goal,j_goal) +1
-                    h_cost = manhattanCost(path.i+1,path.j,i_goal,j_goal)
+                    h_cost = manhattanCost(path.i-1,path.j,i_goal,j_goal)
                     cost = g_cost + h_cost
-                    mat_visited[path.i][path.j] = True
+                    #mat_visited[path.i][path.j] = True
                     queue_infoMat.append(InfoArrElement((path.i-1),(path.j),path,cost))
             #cek kiri
             if(not isIndexOutOfRange(path.i,path.j-1,mat)):
                 if (mat[path.i][path.j-1] == '0' and mat_visited[path.i][path.j-1] == False):
                     g_cost = path.cost - manhattanCost(path.i,path.j,i_goal,j_goal) +1
-                    h_cost = manhattanCost(path.i+1,path.j,i_goal,j_goal)
+                    h_cost = manhattanCost(path.i,path.j-1,i_goal,j_goal)
                     cost = g_cost + h_cost
-                    mat_visited[path.i][path.j] = True
+                    #mat_visited[path.i][path.j] = True
                     queue_infoMat.append(InfoArrElement((path.i),(path.j-1),path,cost))
 
 def manhattanCost(i,j,i_goal,j_goal):
@@ -128,6 +137,7 @@ def isIndexOutOfRange(i,j,mat):
 
 #Program Utama
 def main():
+    jarak = 0 ; jarak2 = 0
     #pilihan untuk memasukkan maze dari file external atau dari keyboard.
     print("Ketik 1 untuk file eksternal, Ketik 0 dari keyboard: ")
     input_param = int(input())
@@ -156,6 +166,7 @@ def main():
             i_awal = idx_baris
             j_awal = 0
             break
+
     for idx_kolom in range(len(mat[0])):
         if (mat[0][idx_kolom] == '0'):
             i_awal = 0
@@ -168,6 +179,8 @@ def main():
             j_goal = len(mat[idx_baris])-1
             i_goal = idx_baris
             break
+
+
     for idx_kolom in range(len(mat[0])):
         if (mat[len(mat)-1][idx_kolom] == '0'):
             i_goal = len(mat)-1
@@ -177,28 +190,36 @@ def main():
     #lakukan BFS
     path = BFS(mat, mat_visitedBFS, i_awal , j_awal, i_goal, j_goal)
     #Dapatkan Path BFS
-    getPath(mat,path)
+    jarak1 = getPath(mat,path,jarak)
 
     queue_infoMat.clear()
-
+    jarak_2 = 0
     #lakukan A-Star
     path2 = Astar(mat2, mat_visitedAstar, i_awal , j_awal, i_goal, j_goal)
     #Dapatkan Path A-Star
-    getPath(mat2,path2)
+    jarak2 = getPath(mat2,path2,jarak_2)
 
+    queue_infoMat.clear()
     if (mat[i_awal][j_awal] == '2'):
         print("BFS :")
         visualization(mat, i_awal, i_goal, j_awal, j_goal)
+        print("jarak adalah:")
+        print(jarak1)
         print("A-STAR :")
         visualization(mat2, i_awal, i_goal, j_awal, j_goal)
+        print("jarak adalah:")
+        print(jarak2)
     else:
         print("TIDAK ADA SOLUSI")
 
-def getPath(mat, path):
+def getPath(mat, path,jarak):
     while (path != None):
         #Jalan yang sudah dikunjungi diganti dengan 2
         mat[path.i][path.j] = '2'
         path = path.parent
+        jarak = jarak + 1
+    return jarak-1
+
 
 def visualization(mat, i_awal, i_goal, j_awal, j_goal):
     init(convert=True)
